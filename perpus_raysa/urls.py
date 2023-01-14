d69e700c918f065451db44fa8857a48cb16c557e
raysasitiaisyah <119478940+raysasitiaisyah@users.noreply.github.com>
2023-01-14 09:45:34 +0700

    Add files via upload

---
+++ perpus_raysa/urls.py
@@ -0,0 +1,32 @@
+"""perpus_raysa URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from perpustakaan.views import *
+from django.contrib.auth.views import LoginView, LogoutView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('buku/', buku, name='buku'),
+    path('penerbit/', penerbit, name='penerbit'),
+    path('', home),
+    path('tambah-buku/', tambah_buku, name='tambah_buku'),
+    path('buku/ubah/<int:id_buku>', ubah_buku, name='ubah_buku'),
+    path('buku/hapus/<int:id_buku>', hapus_buku, name='hapus_buku'),
+    path('masuk/', LoginView.as_view(), name='masuk'),
+    path('keluar/', LogoutView.as_view(next_page='masuk'), name='keluar'),
+    path('signup/', signup, name='signup'),
+]
